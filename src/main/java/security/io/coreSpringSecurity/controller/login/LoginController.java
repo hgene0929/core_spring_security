68bc51c5ce4460714c6938821941559107493cf1
hgene0929 <hgene0929@gmail.com>
2023-01-11 16:32:30 +0900

    :sparkles: 로그아웃 및 인증에 따른 화면 보안 처리

--- src/main/java/security/io/coreSpringSecurity/controller/login/LoginController.java
+++ src/main/java/security/io/coreSpringSecurity/controller/login/LoginController.java
@@ -1,7 +1,14 @@
 package security.io.coreSpringSecurity.controller.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 @Controller
 public class LoginController {
@@ -9,5 +16,17 @@ public class LoginController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    /*
+    * SecurityContextLogoutHandler을 활용한 GET 방식 로그아웃
+    * */
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (Objects.nonNull(authentication)) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/login";
     }
 }
