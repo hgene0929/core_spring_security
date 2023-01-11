81e55122e046b0886c2011211517c211eb88c52b
hgene0929 <hgene0929@gmail.com>
2023-01-11 17:45:57 +0900

    :sparkles: 인증 실패 핸들러

--- src/main/java/security/io/coreSpringSecurity/controller/login/LoginController.java
+++ src/main/java/security/io/coreSpringSecurity/controller/login/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,8 +14,13 @@ import java.util.Objects;
 @Controller
 public class LoginController {
 
+    /*
+    * 로그인 실패시, 핸들러에서 넘겨준 에러관련 파라미터를 뷰로 넘겨주기 위함
+    * */
     @GetMapping("/login")
-    public String login() {
+    public String login(String error, String exception, Model model) {
+        model.addAttribute("error", error);
+        model.addAttribute("exception", exception);
         return "login";
     }
 
--- src/main/java/security/io/coreSpringSecurity/security/config/SecurityConfig.java
+++ src/main/java/security/io/coreSpringSecurity/security/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import security.io.coreSpringSecurity.security.provider.CustomAuthenticationProvider;
 
@@ -25,6 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
     private final AuthenticationDetailsSource authenticationDetailsSource;
     private final AuthenticationSuccessHandler successHandler;
+    private final AuthenticationFailureHandler failureHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -57,7 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/users").permitAll()
+                .antMatchers("/", "/users", "user/login/**", "/login/**").permitAll()
                 .antMatchers("/mypage").hasRole("USER")
                 .antMatchers("/messages").hasRole("MANAGER")
                 .antMatchers("/config").hasRole("ADMIN")
@@ -69,6 +71,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticationDetailsSource(authenticationDetailsSource)
                 .defaultSuccessUrl("/")
                 .successHandler(successHandler)
+                .failureHandler(failureHandler)
                 .permitAll();
     }
 }
